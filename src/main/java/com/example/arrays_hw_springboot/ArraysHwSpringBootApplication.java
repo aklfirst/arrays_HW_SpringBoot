9361363db49217cb142de705b36ed6a672ef0a7e
Alexander Klepov <a_klepov@mail.ru>
2022-09-19 19:32:53 +0300

    Initial commit

---
+++ src/main/java/com/example/arrays_hw_springboot/ArraysHwSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.arrays_hw_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArraysHwSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArraysHwSpringBootApplication.class, args);
+	}
+
+}
